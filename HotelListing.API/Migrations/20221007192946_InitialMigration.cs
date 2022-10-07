04cd135d8f55c020f1b1b63a566c0826a9960407
Alejandra <aleba29@hotmail.com>
2022-10-07 14:47:17 -0600

    Added EntityFramework and Created Database.

---
+++ HotelListing.API/Migrations/20221007192946_InitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Counries_CountryId",
+                table: "Hotels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Counries",
+                table: "Counries");
+
+            migrationBuilder.RenameTable(
+                name: "Counries",
+                newName: "Countries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Countries_CountryId",
+                table: "Hotels",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Countries_CountryId",
+                table: "Hotels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries");
+
+            migrationBuilder.RenameTable(
+                name: "Countries",
+                newName: "Counries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Counries",
+                table: "Counries",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Counries_CountryId",
+                table: "Hotels",
+                column: "CountryId",
+                principalTable: "Counries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
